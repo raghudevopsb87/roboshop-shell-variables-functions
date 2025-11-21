ebba21fbbffb0031ef47daec57d7169cfbd870ad
Raghu K <raghuk.vit@gmail.com>
2025-11-21 07:23:52 +0530

    Variables intro

--- shipping.sh
+++ shipping.sh
@@ -3,6 +3,9 @@ component=shipping
 java_setup
 
 dnf install mysql -y
-mysql -h mysql-dev.rdevopsb87.online -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.rdevopsb87.online -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.rdevopsb87.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+for sqlfile in schema app-user master-data; do
+  mysql -h mysql-dev.rdevopsb87.online -uroot -pRoboShop@1 < /app/db/${sqlfile}.sql
+done
+
+
